9003404afc8cbf4fe4b0591cba9061b4340ce7fc
SmartHardyz <66152035+SmartHardyz@users.noreply.github.com>
2026-02-05 17:52:16 +0700

    Change SQL Server ODBC driver version to 17

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'HOST': 'pisama-admin.database.windows.net',
         'PORT': '1433',
         'OPTIONS': {
-            'driver': 'ODBC Driver 18 for SQL Server',
+            'driver': 'ODBC Driver 17 for SQL Server',
         },
     },
 }
